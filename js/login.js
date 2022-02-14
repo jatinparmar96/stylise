7f0768cf90db4212973dba3b14906ff45745996e
Roberto Guevara <robertoeguevara@gmail.com>
2022-02-13 19:31:15 -0800

    add firebase configuration test code

--- js/login.js
+++ js/login.js
@@ -1,5 +1,25 @@
 // ************Firebase Configuration******************************************
 // ****************************************************************************
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-analytics.js";
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCLJath-3yXRUr4tUdbUgtZBmETHbabxvo",
+//     authDomain: "stylisetest.firebaseapp.com",
+//     projectId: "stylisetest",
+//     storageBucket: "stylisetest.appspot.com",
+//     messagingSenderId: "622177594893",
+//     appId: "1:622177594893:web:a60c538bf60a9bb0902f44",
+//     measurementId: "G-NCQYCRFE5L"
+//   };
+
+//   import {getDatabase, ref, set, child, get, update, remove}
+//     from "https://www.gstatic.com/firebasejs/9.6.6/firebase-database.js"
+
+//     const app = initializeApp(firebaseConfig);
+//     const analytics = getAnalytics(app);
+
+//     const db = getDatabase();
 
 // ************HTML References*************************************************
 // ****************************************************************************
@@ -13,10 +33,12 @@ login.addEventListener('click', ()=>{
 // ************Input validation************************************************
 // ****************************************************************************
 function isEmptyOrSpaces(str){
+    //returns true if username or password field inputs are null or only contains spaces
     return str === null || str.match(/^ *$/) !== null;
 }
 
 function ValidateInput(){
+    //passes users inputs to isEmptyOrSpaces function
     if (isEmptyOrSpaces(username.value) || isEmptyOrSpaces(password.value)){
         alert("Please don't leave any field empty");
     }
@@ -28,25 +50,31 @@ function ValidateInput(){
 // ************User Authentication Process*************************************
 // ****************************************************************************
 function AuthenticateUser(){
-    const dbref = ref(db);
-
+    const dbRef = ref(db);
+    //get snapshot of UsersList database and verifies if user inputs match 
     get(child(dbRef, "UsersList/"+ username.value)).then((snapshot)=>{
         if(snapshot.exists()){
-            let dbpassword = decPass(snapshot.val().password);
+            let dbpassword = snapshot.val().password;
             if(dbpassword == password.value){
-                login();
+                userLogin();
             } else {
-                alert("Wrong username");
+                alert("Wrong password");
             }
         } else {
-            alert("username or password is invalid");
+            alert("Username is invalid");
         }
     });
 }
 
 // ************Password Decription process*************************************
 // ****************************************************************************
-function decPass(dbpassword){
-    let pass = CryptoJS.AES.decrypt(dbpassword, password.value);
-    return pass.toString(CryptoJS.enc.Utf8);
+// function decPass(dbpassword){
+//     let pass = CryptoJS.AES.decrypt(dbpassword, password.value);
+//     return pass.toString(CryptoJS.enc.Utf8);
+// }
+
+//*************Login function*************************************************/
+//****************************************************************************/
+function userLogin(){
+    alert("Login successful!");
 }
