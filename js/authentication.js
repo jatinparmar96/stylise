32ee6e6c51846eb602e1daa6d420f4462b179482
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2022-02-15 20:43:29 -0800

    test authentication

--- js/authentication.js
+++ js/authentication.js
@@ -5,20 +5,20 @@
 //************************************************************
 
 /*************************************************************
-  Firebase Config
+ Firebase Config
  *************************************************************/
 if (!window.firebase) {
     console.log('firebase not loaded');
-  } 
+}
 // Web app's Firebase configuration
-  const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyDoDlxPRl9H3zE2Sbb2bfass8SGHQBE9Lo",
     authDomain: "stylise-1f1d8.firebaseapp.com",
     projectId: "stylise-1f1d8",
     storageBucket: "stylise-1f1d8.appspot.com",
     messagingSenderId: "615569272553",
     appId: "1:615569272553:web:d921d89b8b4eb7093eca78"
-  };
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -26,10 +26,12 @@ firebase.initializeApp(firebaseConfig);
 // Firebase Authentication methods
 const auth = firebase.auth();
 
-const errorElem = document.getElementById('error');
+//Current User
+let currentUser;
 
+const errorElem = document.getElementById('error');
 /*************************************************************
-  SignUp
+ SignUp
  *************************************************************/
 
 // Handle signup button
@@ -44,48 +46,64 @@ function emailSignUp() {
     const txtEmail = document.getElementById('email');
     const txtPassword = document.getElementById('password');
     const txtConfPassword = document.getElementById('conf-password');
-  
-    if (txtPassword.value === txtConfPassword.value){
+
+    if (txtPassword.value === txtConfPassword.value) {
         auth
-        .createUserWithEmailAndPassword(txtEmail.value, txtPassword.value)
-        .then(function () {
-          window.location.href = 'signup.html';
-        })
-        .catch((error) => {
-          console.log('Error in Sign Up : ' + error.code);
-          errorElem.innerText = error.message;
-        });
-    }
-    else {
+            .createUserWithEmailAndPassword(txtEmail.value, txtPassword.value)
+            .then(function () {
+                window.location.href = 'signup.html';
+            })
+            .catch((error) => {
+                console.log('Error in Sign Up : ' + error.code);
+                errorElem.innerText = error.message;
+            });
+    } else {
         errorElem.innerText = "Those passwords didn't match. Try again.";
     }
 
-  }
+}
+
 /*************************************************************
-  LogIn
+ LogIn
  *************************************************************/
 
 // Handle login button
-  document.getElementById('login')?.addEventListener('click', emailLogIn);
+document.getElementById('login')?.addEventListener('click', emailLogIn);
 
 /**
  * Email log up (firebase auth)
  * @method emailLogIn
  */
-  function emailLogIn() {
+function emailLogIn() {
     const txtEmail = document.getElementById('email');
     const txtPassword = document.getElementById('password');
-  
-    auth
-      .signInWithEmailAndPassword(txtEmail.value, txtPassword.value)
-      .then((cred) => {
-        window.location.href = 'login.html';
-      })
-      .catch((error) => {
-        console.log('Error in Log In : ' + error.code);
-        errorElem.innerText = error.message;
-      });
-  }
+
+    auth.signInWithEmailAndPassword(txtEmail.value, txtPassword.value)
+        .then((cred) => {
+            console.log(cred);
+            // window.location.href = 'login.html';
+        })
+        .catch((error) => {
+            console.log('Error in Log In : ' + error.code);
+            errorElem.innerText = error.message;
+        });
+}
+
+/**
+ * Returns the current user object else returns null.
+ * @param void
+ * @returns UserObject | null
+ */
+export function getCurrentUser() {
+    return currentUser;
+}
+
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        currentUser = user;
+    } else {
+    }
+});
   
 
 
