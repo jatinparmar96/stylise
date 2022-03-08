2263fc271cbc7b9817a201134bc48ce533e55f46
Aleirbag04 <94083464+Aleirbag04@users.noreply.github.com>
2022-03-07 18:46:05 -0800

    community add asked changes

--- js/community/community.js
+++ js/community/community.js
@@ -1,13 +1,13 @@
 function init() {
-  
+    //When load show For You by default
     showForYou();
-
+    //Handle Community All
     const all = document.getElementById('community-all')
     all.addEventListener('click', showAllPosts);
-
+     //Handle Community Donate
     const donate = document.getElementById('community-donate')
     donate.addEventListener('click', showDonatePosts);
-
+     //Handle Community For You
     const forYou = document.getElementById('community-for-you')
     forYou.addEventListener('click', showForYou);
    
@@ -20,7 +20,6 @@ function init() {
 function clearWrapper(){
     const wrapper = document.getElementById("wrapper");
     wrapper.innerHTML="";
-
 }
 
 /**
@@ -31,25 +30,19 @@ function clearWrapper(){
 function addPosts(doc){
     let div= document.createElement("div");
     div.classList.add("post");
-
         let img = document.createElement("img");
         img.src = doc.data().uri;
         div.appendChild(img);
-
     let div_user = document.createElement("div");
     div_user.classList.add("user-info");
-
         let img_user = document.createElement("img");
         img_user.classList.add("dp");
         img_user.src = doc.data().user_uri;
         div_user.appendChild(img_user);
-
         let username = document.createElement("span");
         username.innerHTML = doc.data().username;
         div_user.appendChild(username);
-
     div.appendChild(div_user);
-    
     document.getElementById("wrapper").appendChild(div);
 }
 
@@ -59,20 +52,16 @@ function addPosts(doc){
  */
 async function showForYou() {
     const user = await getCurrentUser();
-
+    // Get user tags
     const userFieldsRef = await db.collection('users').doc(user.uid).get();
-   
         const userFields = userFieldsRef.data();
         tags = userFields.tags;
-
     clearWrapper();
-
+    // fetch posts from "posts" collection with conditions
     db.collection("posts").where("userID", "!=", user.uid).where("public", "==", true).where("type", "==", "community").where("keywords", "array-contains-any", tags)
         .get().then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
-            
                addPosts(doc);
-                
             });
     });
         
@@ -84,15 +73,12 @@ async function showForYou() {
  * @method showAllPosts
  */
 function showAllPosts() {
-
     clearWrapper();
-
-    db.collection("posts").where("public", "==", true).where("type", "==", "community")
+    // fetch all posts from "posts" collection
+    db.collection("posts").where("userID", "!=", user.uid).where("public", "==", true).where("type", "==", "community")
         .get().then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
-            
                 addPosts(doc);
-                
             });
     });
 }
@@ -102,15 +88,12 @@ function showAllPosts() {
  * @method showDonatePosts
  */
  function showDonatePosts() {
-
     clearWrapper();
-
-    db.collection("posts").where("public", "==", true).where("type", "==", "donate-item")
+    // fetch posts from "posts" collection with type = donate-item
+    db.collection("posts").where("userID", "!=", user.uid).where("public", "==", true).where("type", "==", "donate-item")
         .get().then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
-            
                 addPosts(doc);
-                
             });
     });
 }
