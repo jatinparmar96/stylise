1064914089ecda2eb7d421892aea589608402608
Aleirbag04 <94083464+Aleirbag04@users.noreply.github.com>
2022-03-07 17:43:59 -0800

    for you: add user filter

--- js/community/community.js
+++ js/community/community.js
@@ -1,45 +1,83 @@
 function init() {
+  
+    showForYou();
 
     const all = document.getElementById('community-all')
     all.addEventListener('click', showAllPosts);
 
     const donate = document.getElementById('community-donate')
     donate.addEventListener('click', showDonatePosts);
-    
-    getTags();
 
-
-
-
-
-    
+    const forYou = document.getElementById('community-for-you')
+    forYou.addEventListener('click', showForYou);
+   
 }
 
+/**
+ * clears main before showing posts
+ * @method clearWrapper
+ */
+function clearWrapper(){
+    const wrapper = document.getElementById("wrapper");
+    wrapper.innerHTML="";
 
-function getTags() {
-    let tagsArr = [];
-    firebase.auth().onAuthStateChanged(user => {
-     let uid = user.uid;
-    db.collection("users").doc(uid)
-        .get().then((doc)=> {
-            let tags =doc.data().tags;
-            tagsArr = tags.split(" ");
-            console.log(tagsArr);
-            
-            
-        })
-
-    })
-    
 }
+
+/**
+ * Show posts that are tagged with account keywords
+ * @method showForYou
+ */
+async function showForYou() {
+    const user = await getCurrentUser();
+
+    const userFieldsRef = await db.collection('users').doc(user.uid).get();
+   
+        const userFields = userFieldsRef.data();
+        tags = userFields.tags;
+
+    clearWrapper();
+
+    db.collection("posts").where("userID", "!=", user.uid).where("public", "==", true).where("type", "==", "community").where("keywords", "array-contains-any", tags)
+        .get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+            
+                let div= document.createElement("div");
+                div.classList.add("post");
+
+                    let img = document.createElement("img");
+                    img.src = doc.data().uri;
+                    div.appendChild(img);
+
+                let div_user = document.createElement("div");
+                div_user.classList.add("user-info");
+
+                    let img_user = document.createElement("img");
+                    img_user.classList.add("dp");
+                    img_user.src = doc.data().user_uri;
+                    div_user.appendChild(img_user);
+
+                    let username = document.createElement("span");
+                    username.innerHTML = doc.data().username;
+                    div_user.appendChild(username);
+
+                div.appendChild(div_user);
+                
+                document.getElementById("wrapper").appendChild(div);
+                
+            });
+    });
+        
+
+ }
 
 /**
  * Fetch all public-posts type community
  * @method showAllPosts
  */
 function showAllPosts() {
-    const wrapper = document.getElementById("wrapper");
-    wrapper.innerHTML="";
+
+    clearWrapper();
+
     db.collection("posts").where("public", "==", true).where("type", "==", "community")
         .get().then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
@@ -76,8 +114,9 @@ function showAllPosts() {
  * @method showDonatePosts
  */
  function showDonatePosts() {
-    const wrapper = document.getElementById("wrapper");
-    wrapper.innerHTML="";
+
+    clearWrapper();
+
     db.collection("posts").where("public", "==", true).where("type", "==", "donate-item")
         .get().then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
@@ -108,7 +147,5 @@ function showAllPosts() {
             });
     });
 }
-
-init();
 
 init();
