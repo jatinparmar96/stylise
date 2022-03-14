bb0e885dbe232218c4e9e2d451f25308235dc2ba
Aleirbag04 <94083464+Aleirbag04@users.noreply.github.com>
2022-03-14 01:43:35 -0700

    user-profile fetch user posts

--- js/profile/user-profile.js
+++ js/profile/user-profile.js
@@ -1,7 +1,31 @@
 function init() {
-    console.log("test");
     userDetails();
+    showUserPosts();
+    console.log("test");
+    //Handle User Outfit Posts
+    const outfit = document.getElementById('profile-outfit')
+    outfit.addEventListener('click', showUserPosts);
+    //Handle User Donate Posts
+    const donate = document.getElementById('profile-donate')
+    donate.addEventListener('click', showUserDonations);
 
+}
+
+/**
+ * identify active post-type tab
+ */
+// Get the container element
+let btnContainer = document.getElementById("post-type-nav");
+console.log(btnContainer);
+// Get all buttons with class="nav-btn" inside the container
+let btns = btnContainer.getElementsByClassName("nav-btn");
+// Loop through the buttons and add the active class to the current/clicked button
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        let current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
 }
 
 async function userDetails() {
@@ -19,7 +43,7 @@ if (userFieldsRef.exists) {
         image.classList.add('profile-image');
     }
 
-    db.collection("posts").where("userID", "==", user.uid).where("type", "==", "community")
+    db.collection("posts").where("userID", "==", user.uid)
     .get().then((querySnapshot) => {
         qPosts.innerHTML = querySnapshot.size;
 
@@ -27,5 +51,64 @@ if (userFieldsRef.exists) {
  
 }
 }
+
+/**
+ * add user posts into the wrapper
+ * @method addUserPosts
+ * @param
+ */
+ function addUserPosts(doc) {
+    let div = document.createElement("div");
+    div.classList.add("post");
+    const link = document.createElement('a');
+    link.href = `index.html#view-post?id=${doc.id}`;
+    let img = document.createElement("img");
+    img.src = doc.data().uri;
+    link.appendChild(img)
+    div.appendChild(link);
+    document.getElementById("wrapper").appendChild(div);
+
+ }
+
+ /**
+ * clears main before showing posts
+ * @method clearWrapper
+ */
+function clearWrapper() {
+    const wrapper = document.getElementById("wrapper");
+    wrapper.innerHTML = "";
+}
+
+
+async function showUserPosts() {
+    const user = await getCurrentUser();
+    clearWrapper();
+    // fetch posts from "posts" collection with conditions
+    db.collection("posts").where("userID", "==", user.uid).where("type", "==", "community")
+        .get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                addUserPosts(doc);
+            });
+        });
+
+}
+
+async function showUserDonations() {
+    const user = await getCurrentUser();
+    clearWrapper();
+    // fetch posts from "posts" collection with conditions
+    db.collection("posts").where("userID", "==", user.uid).where("type", "==", "donate-item")
+        .get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                addUserPosts(doc);
+            });
+        });
+
+}
+
+
+
+
+
 
 init();
