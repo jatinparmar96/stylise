39d7e9df906f4ad240112bad566e5363b9c1bea6
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2022-03-16 11:11:12 -0700

    Remove todo

--- js/utils.js
+++ js/utils.js
@@ -2,9 +2,6 @@
  * Handle the coordinates received from geolocation and convert them to city.
  * Uses Open Street Map Api reverse geolocation api.
  * Documentation can be found here https://nominatim.org/release-docs/develop/api/Reverse/
- * TODO:
- * Save the geolocation coords in firebase
- * 
  * @param {GeolocationPosition} position;
  * @return {void}
  *  */
