1c8ba6f29b0f9ee90700abd0e38ffe5a26979dfc
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2022-03-22 23:33:04 -0700

    initial homepage styling

--- js/landing.js
+++ js/landing.js
@@ -1,3 +1,8 @@
 function init() {
 
 }
+
+function scrollToAbout() {
+    const about = document.getElementById('about-section');
+    about.scrollIntoView({ behavior: 'smooth', block: 'center' })
+}
